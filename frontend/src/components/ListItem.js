71075473476b809be5cbac84cd791e467619d3e4
Rok Zabret <zabret.rok14@gmail.com>
2022-06-09 19:51:29 +0200

    First commit

---
+++ frontend/src/components/ListItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+let getTimestamp = (note) => {
+  return new Date(note.__updatedtime__).toLocaleDateString()
+}
+
+let trimmedContent = (note) => {
+      let content = note.body
+
+      if (content.length > 45) {
+        return content.slice(0, 45) + '...'
+      } else {
+          return content
+      }
+}
+
+const ListItem = ({note}) => {
+  return (
+    <Link to={`/${note.id}`}>
+      <div className="notes-list-item">
+        <h3>{trimmedContent(note)}</h3>
+        <p><span>{getTimestamp(note)}</span></p>
+      </div>
+    </Link>
+  )
+}
+
+export default ListItem
---
+++ frontend/src/pages/Note.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect} from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+import { ReactComponent as SaveIcon } from '../assets/save.svg'
+
+const Note = () => {
+  let params = useParams()
+  let noteId = params.id
+
+  let navigate = useNavigate()
+
+  let [note, setNote] = useState(null)
+
+  useEffect(() => {
+    if(noteId !== 'add') getNote()
+  }, [noteId])
+
+
+  let getNote = async () => {
+    let response = await fetch(`http://localhost:8000/notes/${params.id}`)
+    let data = await response.json()
+    setNote(data)
+  }
+
+  let submitData = async (e) => {
+    e.preventDefault()
+
+
+    let url = 'http://localhost:8000/notes/'
+    let method = 'POST'
+
+    if (params.id !== 'add'){
+        url = `http://localhost:8000/notes/${params.id}`
+        method = 'PUT'
+    }
+
+    let noteBody = note?.body
+    if (noteBody !== undefined){
+      noteBody = String(noteBody).trim()
+    }
+
+    if(noteBody === '' || noteBody === undefined){
+      alert('Note cannot be empty.')
+      return 
+    }
+
+    await fetch(url, {
+        method:method,
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({"body":noteBody})
+    })
+
+    navigate('/')
+  }
+
+  let deleteNote = async (e) => {
+      e.preventDefault()
+      await fetch(`http://localhost:8000/notes/${params.id}`, {method:'DELETE'})
+      navigate('/')
+
+  }
+
+  return (
+    <div className="note">
+      <div className='note-header'>
+        <h3>
+            <Link to="/">
+                <ArrowLeft />
+            </Link>
+        </h3>
+
+        {noteId != 'add' && <button onClick={deleteNote}>Izbris</button>}
+      </div>
+
+      <textarea onChange={(e) => { setNote({ ...note, 'body': e.target.value}) }} placeholder="Edit note" value={note?.body} required></textarea>
+
+      <div onClick={submitData} className="floating-button">
+            <SaveIcon  />
+        </div>
+    </div>
+  )
+}
+
+export default Note
